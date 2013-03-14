66da6556ca63dfee147a2ade0ceaacb883126def
thekraven <savethestyle1@gmail.com>
2013-03-14 02:16:25 -0500

    fix audio build
    Change-Id: I3e59c4fc24a065da127b60f76fed499376355801

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES := libmedia_helper
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy    
