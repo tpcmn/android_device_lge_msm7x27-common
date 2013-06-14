35b1b95c56648a388261cce2a31cad908c973412
thekraven <savethestyle1@gmail.com>
2013-06-13 19:09:17 -0500

    change to 3.0 kernel headers
    Change-Id: I87918c92f0e1f1fdd5d6bce7bbbbb267f1e917e8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,14 +24,25 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 ## Kernel
+## Kernel
+#BUILD_WITH_30X_KERNEL := true
+#ifdef BUILD_WITH_30X_KERNEL
+#TARGET_KERNEL_SOURCE := kernel/lge/msm7x27-3.0.x
+## *** Copy LG Kernel Headers here if necessary, DO NOT use Android auto-generated headers *** 
+#TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include3x 
+#else
 #TARGET_KERNEL_SOURCE := kernel/lge/msm7x27
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+## *** Copy LG Kernel Headers here if necessary, DO NOT use Android auto-generated headers *** 
+#TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include 
+#endif
 TARGET_PREBUILT_KERNEL := device/lge/thunderc/kernels/test21/zImage
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x12200000
 BOARD_KERNEL_PAGESIZE := 2048
 
 # Copy LG Kernel Headers here if necessary, DO NOT use Android auto-generated headers
-TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include
+TARGET_SPECIFIC_HEADER_PATH := device/lge/msm7x27-common/include3x
 
 ## Partition Sizes: Fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00700000
